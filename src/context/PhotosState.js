bb0081dc730460897bc22519275f2a401e218879
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-11-23 00:07:19 +0200

    add unique photo in features handling

--- src/context/PhotosState.js
+++ src/context/PhotosState.js
@@ -6,11 +6,18 @@ const PhotosState = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
 
   const handleAddToFavorites = (photoObj) => {
-    setFavorites([...favorites, photoObj]);
+    const isNotUnique = favorites.find((item) => item.id === photoObj.id);
+
+    if (!isNotUnique) {
+      setFavorites([...favorites, photoObj]);
+    } else {
+      alert("The photo is already in the favorites!");
+    }
   };
 
   const handleRemoveFromFavorites = (e, id) => {
     e.preventDefault();
+
     const updatedFavorites = favorites.filter((item) => item.id !== id);
     setFavorites(updatedFavorites);
   };
